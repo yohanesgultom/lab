c6fafaf2f38170acb8603e03bf7a9da6fd3fb879
Yohanes Gultom <yohanes.gultom@gmail.com>
2020-06-26 13:32:08 +0700

    fix icons

--- src/router/index.js
+++ src/router/index.js
@@ -4,55 +4,60 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-    meta: {title: 'Home', icon: 'mdi-home'},
-  },
-  {
-    path: '/passgen',
-    name: 'passgen',
-    component: () => import(/* webpackChunkName: "passgen" */ '../views/PassGen.vue'),
-    meta: {title: 'Password Generator', icon: 'mdi-shield-key'},
-  },
-  {
-    path: '/xlsx',
-    name: 'xlsx',
-    component: () => import(/* webpackChunkName: "xlsx" */ '../views/Xlsx.vue'),
-    meta: {title: 'XLSX Parser', icon: 'mdi-file'},
-  },
-  {
-    path: '/emoji',
-    name: 'emoji',
-    component: () => import(/* webpackChunkName: "emoji" */ '../views/Emoji.vue'),
-    meta: {title: 'Emoji Converter', icon: 'mdi-face'},
-  },
-  {
-    path: '/ethereum',
-    name: 'ethereum',
-    component: () => import(/* webpackChunkName: "ethereum" */ '../views/Ethereum.vue'),
-    meta: {title: 'Ethereum Integration', icon: 'mdi-ethereum'},
-  },
-  {
-    path: '/linkedIn',
-    name: 'linkedIn',
-    component: () => import(/* webpackChunkName: "linkedIn" */ '../views/LinkedIn.vue'),
-    meta: {title: 'LinkedIn Share', icon: 'mdi-linkedin-box'},
-  },
-  {
-    path: '/warta',
-    name: 'warta',
-    component: () => import(/* webpackChunkName: "warta" */ '../views/Warta.vue'),
-    meta: {title: 'Warta Search', icon: 'mdi-file-document-box-search'},
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: { title: 'Home', icon: 'mdi-home' },
+    },
+    {
+        path: '/passgen',
+        name: 'passgen',
+        component: () =>
+            import ( /* webpackChunkName: "passgen" */ '../views/PassGen.vue'),
+        meta: { title: 'Password Generator', icon: 'mdi-shield-key' },
+    },
+    {
+        path: '/xlsx',
+        name: 'xlsx',
+        component: () =>
+            import ( /* webpackChunkName: "xlsx" */ '../views/Xlsx.vue'),
+        meta: { title: 'XLSX Parser', icon: 'mdi-file' },
+    },
+    {
+        path: '/emoji',
+        name: 'emoji',
+        component: () =>
+            import ( /* webpackChunkName: "emoji" */ '../views/Emoji.vue'),
+        meta: { title: 'Emoji Converter', icon: 'mdi-face' },
+    },
+    {
+        path: '/ethereum',
+        name: 'ethereum',
+        component: () =>
+            import ( /* webpackChunkName: "ethereum" */ '../views/Ethereum.vue'),
+        meta: { title: 'Ethereum Integration', icon: 'mdi-ethereum' },
+    },
+    {
+        path: '/linkedIn',
+        name: 'linkedIn',
+        component: () =>
+            import ( /* webpackChunkName: "linkedIn" */ '../views/LinkedIn.vue'),
+        meta: { title: 'LinkedIn Share', icon: 'mdi-linkedin' },
+    },
+    {
+        path: '/warta',
+        name: 'warta',
+        component: () =>
+            import ( /* webpackChunkName: "warta" */ '../views/Warta.vue'),
+        meta: { title: 'Warta Search', icon: 'mdi-file-find' },
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
